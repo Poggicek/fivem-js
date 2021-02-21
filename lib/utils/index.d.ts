e7506ac4bc34d839a3302ec7330acb2e548aab66
Poggu <poggu@seznam.cz>
2021-02-21 16:16:50 +0100

    Fuck npm

---
+++ lib/utils/index.d.ts
@@ -0,0 +1,11 @@
+export { Vector3 } from './Vector3';
+export { String } from './String';
+export { LiteEvent } from './LiteEvent';
+export { PointF } from './PointF';
+export { Crypto } from './Crypto';
+export { Point } from './Point';
+export { Color } from './Color';
+export { Maths } from './Maths';
+export { Size } from './Size';
+export { Quaternion } from './Quaternion';
+export declare const Wait: (milliseconds: number) => Promise<void>;
